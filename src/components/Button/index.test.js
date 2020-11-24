0e11eed4ab9f3bebceb00a27de3b825c3eb19ea9
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2020-11-24 19:24:35 +0700

    test 2

--- src/components/Button/index.test.js
+++ src/components/Button/index.test.js
@@ -8,3 +8,17 @@ test("Should not allowed click button if isDisabled is presen", () => {
     expect(container.querySelector("span.disabled")).toBeInTheDocument();
 });
 
+test("Should render loading/spinner", () => {
+    const {container, getByText} = render(<Button isLoading></Button>);
+
+    expect(getByText(/loading/i)).toBeInTheDocument()
+    expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+    const {container} = render(<Button type="link" isExternal></Button>);
+
+    expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+
