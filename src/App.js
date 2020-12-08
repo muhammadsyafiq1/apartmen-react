6a3d38da11e809224b8a5854ccc8147b5f35850e
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2020-12-08 09:31:21 +0700

    number and date done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "../src/Assets/scss/styles.scss"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingPage from "Pages/LandingPage";
+import Example from "Pages/Example";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Route path="/" component={LandingPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
---
+++ src/Pages/Example.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Component } from 'react';
+import {InputDate , InputNumber} from 'components/Form';
+
+export default class Example extends Component {
+    state = {
+        value: {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection",
+        },
+      };
+    
+    handleChange = (e) => {
+        this.setState({ value: e.target.value });
+    };
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row align-items-center justify-content-center" style={{ height: "100vh" }}>
+                    <div className="col-auto">
+                    <InputDate
+                        max={30}
+                        onChange={this.handleChange}
+                        name="value"
+                        value={this.state.value}
+                    />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
